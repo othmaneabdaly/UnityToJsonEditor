d9fa35cb5cbab872c37d2037a3b06876511018c3
othmaneabdaly <othmaneabdaly@gmail.com>
2019-05-05 21:53:55 +0000

    Edited

--- Assets/Editor/FortnitePlayerEditor.cs
+++ Assets/Editor/FortnitePlayerEditor.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditorInternal;
 
 
-public class AnyGamePlayerEditor
+public class PlayerModelEditor : EditorWindow
 {
-    public override void OnInspectorGUI()
+    PlayerModel p = new PlayerModel();
+    [MenuItem("Window/Editor")]
+    static void Init()
     {
-        AnyGamePlayer player = (AnyGamePlayer)target;
-        EditorGUILayout.TextField("Name:", player.Name);
-        EditorGUILayout.IntField("Health:", player.Health);
-        EditorGUILayout.IntField("Armor:", player.Armor);
+        GetWindow<PlayerModelEditor>("Welcome");
+        PlayerModelEditor window = (PlayerModelEditor)EditorWindow.GetWindow(typeof(PlayerModelEditor));
+        window.maximized = true;
+        window.name = "Welcome";
+        window.Show();
+    }
+    void OnEnable()
+    {
+        List<PlayerModel> listPlayer;
+    }
+    void OnGUI()
+    {
+        List<PlayerModel> listPlayer;
+
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        //ScriptableObject target = this;
+        //SerializedObject so = new SerializedObject(target);
+        //SerializedProperty stringsProperty = so.FindProperty("listPlayer");
+
+        //EditorGUILayout.PropertyField(stringsProperty, true); // True means show children
+        //so.ApplyModifiedProperties();
+        p.Name = EditorGUILayout.TextField("Name: ", p.Name);
+        p.Health = EditorGUILayout.IntField("Health: ", p.Health);
+        p.Armor = EditorGUILayout.IntField("Armor: ", p.Armor);
+        if (GUILayout.Button("Save"))
+        {
+            
+        }
+         
+
+
     }
 }
--- Assets/FortnitePlayer/FortnitePlayer.cs
+++ Assets/FortnitePlayer/FortnitePlayer.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnyGamePlayer
+public class PlayerModel
 {
     public string Name;
     public int Health;
     public int Armor;
-
-    public override string ToString()
-    {
-        return "Name: "+Name+",Health: "+Health+",Armor: "+Armor;
-    }
 }
 public class Players
 {
-    public Dictionary<string, AnyGamePlayer> PlayersList;
+    public Dictionary<string, PlayerModel> PlayersList;
+    public PlayerModel GetPlayerByName(string name)
+    {
+        foreach(KeyValuePair < string, PlayerModel> player in PlayersList)
+        {
+            if(player.Key.Equals(name))
+            {
+                return player.Value;
+            }
+        }
+        return null;
+    }
 }
